aa0e6cd880df3b8cf71e3e839b21d560a66b6751
André Fonseca Leopoldino Alves <andreflalves@gmail.com>
2017-06-26 20:40:24 -0300

    Implementação da leitura da localização no dispositivo

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-#This project was developed by André Fonseca Leopoldino Alves with the mentoring of Profa Dra Simone das Graças Domingues Prado as the final project of my computer science graduation course.
+# This project was developed by André Fonseca Leopoldino Alves with the mentoring of Profa Dra Simone das Graças Domingues Prado as the final project of my computer science graduation course.
 
 # Botão do Pãnico UNESP Bauru
 Panic Button - UNESP Bauru is a safety app for emergency situations that occur inside the campus Bauru of the São Paulo State University.
+
+#General setup
+This application uses NodeJS, so make sure you have that installed: https://nodejs.org/en/download/
+
+#Mobile App Setup
+Install the Ionic Framework & Cordava: npm install -g ionic cordova
+Install geolocation plugin: ionic cordova plugin add cordova-plugin-geolocation
+
+#Running the project 
+npm run ionic:serve
--- mobile/src/pages/home/home.ts
+++ mobile/src/pages/home/home.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Location } from '../../models/location';
+import { Geolocation } from '@ionic-native/geolocation';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  location : Location = {
+    lat: 0,
+    lng: 0
+  };
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private geolocation: Geolocation) {
 
+  }
+
+  onLocate() {
+    this.geolocation.getCurrentPosition()
+      .then(
+        location => {
+          console.log("location = ", location);
+          this.location.lat = location.coords.latitude;
+          this.location.lng = location.coords.longitude;
+        }
+      )
+      .catch(
+        error => {
+          console.log("Erro para obter a localização")
+        } 
+      );
   }
 
 }
